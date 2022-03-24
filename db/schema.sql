200362a91a963bd8bdda2ca2d02bbebd5aba8df9
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-03-24 16:50:43 -0400

    schema update

--- db/schema.sql
+++ db/schema.sql
@@ -4,31 +4,27 @@ CREATE DATABASE employee_db
 USE employee_db
 
 CREATE TABLE department (
-    id INT,
-    [name] VARCHAR(255) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    [name] VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
  CREATE TABLE [role] (
-    id INT,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT,
+    salary DECIMAL,
+    depart_id INT,
     PRIMARY KEY (id)
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)     
-    ON DELETE SET NULL
+    FOREIGN KEY (depart_id) REFERENCES department(id)     
  );
 
 CREATE TABLE employee (
-    id INT,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
     PRIMARY KEY (id)
-    FOREIGN KEY (role_id)
-    REFERENCES [role](id)
-    -- FOREIGN KEY (manager_id)
-    -- REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES [role](id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );

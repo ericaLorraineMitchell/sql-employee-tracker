6613e569a91591a24d8f1b223f2ca4910c1deeb2
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-03-24 16:34:47 -0400

    schema table work

--- db/schema.sql
+++ db/schema.sql
@@ -9,11 +9,12 @@ CREATE TABLE department (
     PRIMARY KEY (id)
 );
 
- CREATE TABLE role (
+ CREATE TABLE [role] (
     id INT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
+    PRIMARY KEY (id)
     FOREIGN KEY (department_id)
     REFERENCES department(id)     
     ON DELETE SET NULL
@@ -25,8 +26,9 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
+    PRIMARY KEY (id)
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    REFERENCES [role](id)
+    -- FOREIGN KEY (manager_id)
+    -- REFERENCES employee(id)
 );
